047d1170271f7af3a19f48dcdc0a917a143f779f
Sheraz Khan <206346817+sherazkdev@users.noreply.github.com>
2025-12-05 15:31:57 +0500

    Add default API base URL for axios instance
    Set default API base URL if environment variable is not defined.

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.VITE_SERVER_URL;
+const API_BASE_URL = process.env.VITE_SERVER_URL || "https://customer-relationship-backend.vercel.app";
+console.log(API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
